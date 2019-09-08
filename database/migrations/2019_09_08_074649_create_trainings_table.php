794faad47db4409f6ff0ce5c4615289451118e82
Harits Rahman <haritsrm@MacBooks-MacBook-Pro.local>
2019-09-08 15:34:54 +0700

    add training data UI and entity state

---
+++ database/migrations/2019_09_08_074649_create_trainings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrainingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->float('pH');
+            $table->float('temperature');
+            $table->float('turbidity');
+            $table->integer('classes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainings');
+    }
+}
---
+++ resources/views/data-training.blade.php
@@ -0,0 +1,26 @@
+hai nurfi
+<table border=1>
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>pH</th>
+            <th>Suhu</th>
+            <th>Kekeruhan</th>
+            <th>Kelas</th>
+        </tr>
+    </thead>
+    <tbody>
+        {{! $i = 1 }}
+        @foreach($trainings as $training)
+        <tr>
+            <td>{{ $i++ }}</td>
+            <td>{{ $training->name }}</td>
+            <td>{{ $training->pH }}</td>
+            <td>{{ $training->temperature }}</td>
+            <td>{{ $training->turbidity }}</td>
+            <td>{{ $training->classes }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
